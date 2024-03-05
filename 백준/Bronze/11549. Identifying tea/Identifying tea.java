c51f341106e96ff2d99e0f2340af3a069d959eb1
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-03-06 02:45:38 +0900

    [Bronze IV] Title: Identifying tea, Time: 204 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int f = sc.nextInt();
+		int count = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			int a = sc.nextInt();
+			if(f == a) {
+				count++;
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
+}
