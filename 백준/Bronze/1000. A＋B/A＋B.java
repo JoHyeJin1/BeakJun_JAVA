8040aaceb514f212d6046166438ea71f758c8627
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-03-06 00:31:00 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+//1000번 A+B
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(a + b);
+		sc.close();
+	}
+}
