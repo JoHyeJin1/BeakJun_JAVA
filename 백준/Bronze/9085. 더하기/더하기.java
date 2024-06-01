5d5e35491509fc54dc3d4c0d52881e75325fc168
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2024-06-01 23:02:17 +0900

    [Bronze III] Title: 더하기, Time: 224 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			int total = 0;
+			
+			for(int j = 0; j < m; j++) {
+				int a = sc.nextInt();
+				total += a;
+			}
+			System.out.println(total);
+		}
+		sc.close();
+	}
+}
