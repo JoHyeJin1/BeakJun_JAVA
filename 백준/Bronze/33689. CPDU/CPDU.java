1e1f1fe1741577a60d137366542a143882415227
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2025-05-04 21:42:53 +0900

    [Bronze IV] Title: CPDU, Time: 176 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/33689. CPDU/CPDU.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int count = 0;
+		for(int i = 0; i < a; i++) {
+			String b = sc.next();
+			if(b.charAt(0) == 'C') {
+				count++;
+			}
+		}
+		System.out.println(count);
+		sc.close();
+	}
+}
