9fee097819e471a96e15858f44276dc2cd429d31
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2025-05-09 04:29:21 +0900

    [Bronze III] Title: What is n, Daddy?, Time: 164 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/6794. What is n， Daddy？/What is n， Daddy？.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		if(n == 1) {
+			System.out.println("1");
+		}else if(n == 2) {
+			System.out.println("2");
+		}else if(n == 3) {
+			System.out.println("2");
+		}else if(n == 4) {
+			System.out.println("3");
+		}else if(n == 5) {
+			System.out.println("3");
+		}else if(n == 6) {
+			System.out.println("3");
+		}else if(n == 7) {
+			System.out.println("2");
+		}else if(n == 8) {
+			System.out.println("2");
+		}else if(n == 9) {
+			System.out.println("1");
+		}else if(n == 10) {
+			System.out.println("1");
+		}
+		sc.close();
+	}
+}
