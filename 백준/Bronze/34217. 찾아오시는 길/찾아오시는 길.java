f12d615e1e7c620def2e2e082871c9da2ffbd4ed
JoHyeJin <121273234+JoHyeJin1@users.noreply.github.com>
2025-09-05 02:25:18 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 176 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		
+		if(a + c < b + d) {
+			System.out.println("Hanyang Univ.");
+		}else if(a + c > b + d) {
+			System.out.println("Yongdap");
+		}else {
+			System.out.println("Either");
+		}
+		sc.close();
+	}
+}
